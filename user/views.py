a68a865a13c543ceeda1c945c10b5ee985befcf8
minchih <cooker0521@gmail.com>
2024-09-28 11:01:02 +0800

    1.完成登入功能 2.完成登出功能 3.優化註冊完進入登入畫面自動填入帳號

--- user/views.py
+++ user/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+
 
 # Create your views here.
 
 
-def index(request):
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+
+def user_profile(request):
+    return render(request, "user/profile.html")
+
+
+def user_login(request):
+    message = ""
+    user = None
+    if request.method == "POST":
+        if request.POST.get("register"):
+            return redirect("register")
+        if request.POST.get("login"):
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            if username == "" or password == "":
+                message = "帳號密碼不能為空!"
+            else:
+                user = authenticate(request, username=username, password=password)
+                if not user:
+                    message = "帳號或密碼錯誤!"
+                else:
+                    login(request, user)
+                    message = "登入成功!"
+                    return redirect("profile")
+
+    return render(request, "user/login.html", {"message": message, "user": user})
+
+
+def user_register(request):
     massage = ""
     form = UserCreationForm()
     # all,get,filter
@@ -31,5 +66,6 @@ def index(request):
                 user = User.objects.create_user(username=username, password=password1)
                 user.save()
                 massage = "註冊成功!"
+                return render(request, "user/login.html", {"user": user})
 
     return render(request, "user/register.html", {"form": form, "message": massage})
