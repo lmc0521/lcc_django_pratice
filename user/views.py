10829f3d944444f39f820fea4100b61247ce0cd5
minchih <cooker0521@gmail.com>
2024-09-28 13:28:04 +0800

    新增session記錄使用者帳號

--- user/views.py
+++ user/views.py
@@ -19,6 +19,7 @@ def user_profile(request):
 def user_login(request):
     message = ""
     user = None
+    username = request.session.get("username", "")
     if request.method == "POST":
         if request.POST.get("register"):
             return redirect("register")
@@ -37,7 +38,11 @@ def user_login(request):
                     message = "登入成功!"
                     return redirect("profile")
 
-    return render(request, "user/login.html", {"message": message, "user": user})
+    return render(
+        request,
+        "user/login.html",
+        {"message": message, "user": user, "username": username},
+    )
 
 
 def user_register(request):
@@ -66,6 +71,7 @@ def user_register(request):
                 user = User.objects.create_user(username=username, password=password1)
                 user.save()
                 massage = "註冊成功!"
-                return render(request, "user/login.html", {"user": user})
+                request.session["username"] = user.username
+                return redirect("login")
 
     return render(request, "user/register.html", {"form": form, "message": massage})
