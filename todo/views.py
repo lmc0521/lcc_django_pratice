50e0fde1330d915eb8d643fb888b4b97bbda89ca
minchih <cooker0521@gmail.com>
2024-10-05 12:00:43 +0800

    完成新增待辦事項功能

--- todo/views.py
+++ todo/views.py
@@ -1,26 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
 
 
 # Create your views here.
+# 新增代辦事項
 def create_todo(request):
     message = ""
-    form = TodoForm()
+    user = request.user
+    form = None
+    if not user.is_authenticated:
+        message = "請先登入"
+    else:
+        form = TodoForm()
+        if request.method == "POST":
+            try:
+                print(request.POST)
+                form = TodoForm(request.POST)
+                todo = form.save(commit=False)
+                todo.user = request.user
+                todo.save()
+                message = "提交成功"
+                return redirect("todolist")
+            except Exception as e:
+                print(e)
+                message = "提交失敗"
 
     return render(request, "todo/create-todo.html", {"form": form, "message": message})
 
 
+# 檢視代辦事項
 def todo(request, id):
     user = request.user
     message = ""
     todo = None
+
     try:
         todo = Todo.objects.get(id=id, user=user)
+        form = TodoForm(instance=todo)
     except Exception as e:
         print(e)
         message = "編號錯誤"
-    return render(request, "todo/todo.html", {"todo": todo, "message": message})
+    return render(
+        request, "todo/todo.html", {"form": form, "todo": todo, "message": message}
+    )
 
 
 def todolist(request):
