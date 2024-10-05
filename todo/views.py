2b856e6842ab0b2b77c90b4715f5218061fadc49
minchih <cooker0521@gmail.com>
2024-10-05 11:22:11 +0800

    使用者表單填寫製作中

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
 
 
 # Create your views here.
+def create_todo(request):
+    message = ""
+    form = TodoForm()
+
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
+
+
+def todo(request, id):
+    user = request.user
+    message = ""
+    todo = None
+    try:
+        todo = Todo.objects.get(id=id, user=user)
+    except Exception as e:
+        print(e)
+        message = "編號錯誤"
+    return render(request, "todo/todo.html", {"todo": todo, "message": message})
+
+
 def todolist(request):
     user = request.user
     todos = None
